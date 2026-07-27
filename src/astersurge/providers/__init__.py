ec647edc7b24d4446a8c2ebaefbe867e2c732a98
Prakhar Srivastava <prakharsrivastavaagi@gmail.com>
2026-07-27 13:19:28 +0530

    Update __init__.py

--- src/astersurge/providers/__init__.py
+++ src/astersurge/providers/__init__.py
@@ -1,9 +1,11 @@
 from .base import BaseProvider
 from .groq import GroqProvider
 from .openai import OpenAIProvider
+from .gemini import GeminiProvider
 
 __all__ = [
     "BaseProvider",
     "GroqProvider",
     "OpenAIProvider",
+    "GeminiProvider",
 ]
